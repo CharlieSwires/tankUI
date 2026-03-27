5e0ad814ac0cda458ac47822fb6c71f5a8035430
CharlieSwires <charlieswires@gmail.com>
2026-03-27 21:01:27 +0000

    Latest

--- src/Main/Telemetry.java
+++ src/Main/Telemetry.java
@@ -1,7 +1,10 @@
 package Main;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,7 +14,7 @@ import Const.Constant;
 
 public class Telemetry {
 	private enum COMMS_STATUS {
-		CPU_GOOD, CPU_DOWN, CPU_COMMS_DOWN, BATTERY_GOOD, BATTERY_DOWN, BATTERY_COMMS_DOWN, CPU_COMMS_TIMEOUT, BATTERY_COMMS_TIMEOUT
+		CPU_GOOD, CPU_DOWN, CPU_COMMS_DOWN, BATTERY_GOOD, BATTERY_DOWN, BATTERY_COMMS_DOWN, CPU_COMMS_TIMEOUT, BATTERY_COMMS_TIMEOUT, BATTERY_COMMS_FINE, CPU_COMMS_FINE
 	}
 	// Creating the JFrame for the application
 	private static JFrame frame = new JFrame("Telemetry");
@@ -21,6 +24,8 @@ public class Telemetry {
 	private static JLabel cpuTitle = new JLabel("CPU OK", SwingConstants.LEFT);
 	private static Color originalColour = cpuTitle.getForeground();
 	private static JLabel batteryTitle= new JLabel("BATTERY OK", SwingConstants.RIGHT);
+	private static JLabel batteryTitle2= new JLabel("BATTERY OK", SwingConstants.RIGHT);
+	private static JLabel cpuTitle2= new JLabel("CPU OK", SwingConstants.LEFT);
 	private static void updateStatus(COMMS_STATUS stat) {
 		switch(stat) {
 		case CPU_DOWN:
@@ -38,8 +43,12 @@ public class Telemetry {
 			cpuTitle.setText("CPU COMMS DOWN");
 			break;
 		case CPU_COMMS_TIMEOUT:
-			cpuTitle.setForeground(Color.RED);
-			cpuTitle.setText("CPU COMMS UP TIMEOUT");
+			cpuTitle2.setForeground(Color.RED);
+			cpuTitle2.setText("CPU COMMS UP TIMEOUT");
+			break;
+		case CPU_COMMS_FINE:
+			cpuTitle2.setForeground(originalColour);
+			cpuTitle2.setText("CPU COMMS UP");
 			break;
 		case BATTERY_DOWN:
 			frame.setForeground(Color.RED);
@@ -52,16 +61,24 @@ public class Telemetry {
 			batteryTitle.setText("BATTERY OK");
 			break;
 		case BATTERY_COMMS_DOWN:
+			frame.setForeground(Color.RED);
 			batteryTitle.setForeground(Color.RED);
 			batteryTitle.setText("BATTERY COMMS DOWN");
 			break;
 		case BATTERY_COMMS_TIMEOUT:
-			batteryTitle.setForeground(Color.RED);
-			batteryTitle.setText("BATTERY COMMS UP TIMEOUT");
+			frame.setForeground(Color.RED);
+			batteryTitle2.setForeground(Color.RED);
+			batteryTitle2.setText("BATTERY COMMS UP TIMEOUT");
+			break;
+		case BATTERY_COMMS_FINE:
+			frame.setForeground(Color.BLACK);
+			batteryTitle2.setForeground(originalColour);
+			batteryTitle2.setText("BATTERY COMMS UP");
 			break;
 		default:
 			throw new RuntimeException("Not a valid status");
 		}
+		frame.repaint();
 	}
 
 
@@ -72,8 +89,6 @@ public class Telemetry {
 			while (true) {
 				Double result = getCPUTemp();
 				Double result2 = getBatteryVoltage();
-				getBatteryTimeout();
-				getCPUTimeout();
 				if ((""+result2).equals(""+Constant.ERROR+".0")){
 				}
 				else {
@@ -86,6 +101,21 @@ public class Telemetry {
 					Thread.sleep(Constant.sensor_read_ms);
 				} catch (InterruptedException e) {
 				}
+
+			}
+		}
+	}
+	private class MyThreadTimeout extends Thread {
+		@Override
+		public void run() {
+			while (true) {
+				getBatteryTimeout();
+				getCPUTimeout();
+				try {
+					Thread.sleep(Constant.sensor_read_ms);
+				} catch (InterruptedException e) {
+				}
+
 			}
 		}
 	}
@@ -99,21 +129,29 @@ public class Telemetry {
 		toppanel.add(cpuTitle);
 		toppanel.add(batteryTitle= new JLabel("BATTERY OK", SwingConstants.RIGHT));
 		frame.add(toppanel, BorderLayout.NORTH);
+		JPanel middlepanel = new JPanel();
+		middlepanel.add(cpuTitle2);
+		middlepanel.add(batteryTitle2);
+		frame.add(middlepanel, BorderLayout.SOUTH);
 
 		// Making the frame visible
 		frame.setVisible(true);
 		Telemetry er = new Telemetry();
 		MyThreadTelemetry t2 = er.new MyThreadTelemetry();
 		t2.start();
+		MyThreadTimeout t = er.new MyThreadTimeout();
+		t.start();
 
 	}
+	private double batteryVoltage2;
+	private Double timeout;
 
 
 	public Double getCPUTemp() {
 		Constant.gg.getGenericAsync(
 				"/telemetry/cpu-temp",
 				result -> {
-					cpuTemp = result != null? result:null;
+					cpuTemp = result;
 					if ((""+cpuTemp).equals(""+Constant.ERROR+".0")) {
 						updateStatus(COMMS_STATUS.CPU_DOWN);
 					} else {
@@ -131,7 +169,7 @@ public class Telemetry {
 		Constant.gg.getGenericAsync(
 				"/telemetry/volts/0",
 				result -> {
-					batteryVoltage = (double)result;
+					batteryVoltage = result;
 					if ((""+batteryVoltage).equals(""+Constant.ERROR+".0")) {
 						updateStatus(COMMS_STATUS.BATTERY_DOWN);
 					} else {
@@ -150,37 +188,39 @@ public class Telemetry {
 		Constant.gg.getGenericAsync(
 				"/telemetry/timeout-temp",
 				result -> {
-					Double timeout = result != null? result:null;
-					if ((""+timeout).equals(""+Constant.ERROR+".0")) {
+					timeout = result;
+					if ((""+Constant.ERROR+".0").equals(""+timeout)) {
 						updateStatus(COMMS_STATUS.CPU_COMMS_TIMEOUT);
 					} else {
-						updateStatus(COMMS_STATUS.CPU_GOOD);
+						updateStatus(COMMS_STATUS.CPU_COMMS_FINE);
 
 					}
 				},
 				errorMessage -> {
-					updateStatus(COMMS_STATUS.CPU_COMMS_DOWN);
+					updateStatus(COMMS_STATUS.CPU_COMMS_TIMEOUT);
 				}
 				);
-		return cpuTemp;
+		System.out.println(timeout);
+		return timeout;
 	}
 	public Double getBatteryTimeout() {
 		Constant.gg.getGenericAsync(
 				"/telemetry/timeout-battery",
 				result -> {
-					batteryVoltage = (double)result;
-					if ((""+batteryVoltage).equals(""+Constant.ERROR+".0")) {
+					batteryVoltage2 = result;
+					if ((""+Constant.ERROR+".0").equals(""+batteryVoltage2)) {
 						updateStatus(COMMS_STATUS.BATTERY_COMMS_TIMEOUT);
 					} else {
-						updateStatus(COMMS_STATUS.BATTERY_GOOD);
+						updateStatus(COMMS_STATUS.BATTERY_COMMS_FINE);
 
 					}
 				},
 				errorMessage -> {
-					updateStatus(COMMS_STATUS.BATTERY_COMMS_DOWN);
+					updateStatus(COMMS_STATUS.BATTERY_COMMS_TIMEOUT);
 				}
 				);
-		System.out.println(batteryVoltage);
-		return batteryVoltage;
+		System.out.println(batteryVoltage2);
+
+		return batteryVoltage2;
 	}
 }
